3d169a47203d5b73dea3e7a75b68a89099ecc4be
abidmujkic <abidmujkic1@gmail.com>
2021-09-15 22:37:20 +0200

    ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // This is single line comment
+
+    /*document.write("This is a multi-line comment!"); 
+    document.write("This is a multi-line comment!"); 
+    document.write("This is a multi-line comment!"); 
+    document.write("This is a multi-line comment!"); 
+    document.write("This is a multi-line comment!"); 
+    */
+}
+console.log(main());
+module.exports = main;
