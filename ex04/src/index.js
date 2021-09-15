0a6f98241f063c8d3e325a8eb91ab3343f8beeb7
abidmujkic <abidmujkic1@gmail.com>
2021-09-15 22:53:25 +0200

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    x = 10;
+    y = x;
+    // Only Change code above this line
+    return [x, y];
+}
+console.log(main());
+module.exports = main;
