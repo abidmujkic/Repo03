0fb640b3b97cfb65c5dc1a7bfea7f3732f676825
abidmujkic <abidmujkic1@gmail.com>
2021-09-15 23:36:52 +0200

    ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+
+   var difference = numOne - numTwo;
+   
+
+  
+    // Only Change code above this line
+    
+    return difference;
+       
+}
+
+
+console.log(main(425, 120)); // change this line
+
+module.exports = main;
